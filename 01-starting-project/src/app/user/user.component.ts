ef1df1705b9f56301ece16b73e8aabfbbc2dfa1b
Pantelii Max <pantelejmax@gmail.com>
2025-02-15 13:38:45 +0200

    finished first course section (learned about services and local data storage), added some models.

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,24 +1,20 @@
-import { Component, EventEmitter, Input, Output, output} from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
-
-interface User {
-    id: string;
-    avatar: string;
-    name: string;
-}
+import { Component, EventEmitter, Input, Output, output } from '@angular/core';
+import { User } from './user.model';
+import { CardComponent } from '../shared/card/card.component';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [], 
+  imports: [CardComponent],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({required: true}) user!: User; 
+  @Input({ required: true }) user!: User;
+  @Input({ required: true }) isSelected!: boolean;
   @Output() select = new EventEmitter<string>();
 
-  get imagePath(){
+  get imagePath() {
     return 'assets/users/' + this.user.avatar;
   }
 
